a432bdb68e2b1fd06fd3e42b494d174c2019706e
smartvain <zlc.blue15210@gmail.com>
2022-11-11 18:37:51 +0900

    add: Layout component

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+
+const logout = async () => {
+  client.auth.signOut()
+}
+</script>
+
+<template>
+  <header class="flex justify-between py-2 px-4">
+    <h1 class="font-semibold">Nuxt 3 + SupaBase Auth</h1>
+    <button
+      class="px-4 py-2 bg-slate-400 text-white rounded"
+      @click="logout"
+    >
+      <span> Log out </span>
+    </button>
+  </header>
+  <slot />
+</template>
