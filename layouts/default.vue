27824b1ec4b7a3b16dadc9c63acc7c68a458a9b5
smartvain <zlc.blue15210@gmail.com>
2022-11-11 18:49:55 +0900

    add: judge login

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
+const isLogin = ref(false)
 const client = useSupabaseClient()
 
 const logout = async () => {
   client.auth.signOut()
 }
+
+const user = useSupabaseUser()
+onMounted(() => {
+  watchEffect(() => {
+    isLogin.value = !!user.value
+  })
+})
 </script>
 
 <template>
-  <header class="flex justify-between py-2 px-4">
+  <header class="flex justify-between items-center py-2 px-4">
     <h1 class="font-semibold">Nuxt 3 + SupaBase Auth</h1>
     <button
-      class="px-4 py-2 bg-slate-400 text-white rounded"
+      v-if="isLogin"
+      class="px-4 py-2 bg-slate-500 text-white rounded"
       @click="logout"
     >
       <span> Log out </span>
